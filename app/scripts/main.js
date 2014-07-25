5e0d0c8e946250b303d40dd34e2d6006366931c1
Perrine BOISSIERES <perrine.boissieres@awstudio.fr>
2014-07-25 15:59:25 +0200

    Tests callbacks + capture d'image

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,65 @@
 $( document ).ready(function() {
 
-	// Récupération d'une vidéo en local et affichage
-    jwplayer("video").setup({
+	// Récupération d'une vidéo en local
+    var playerInstance = jwplayer("video").setup({
+    	flashplayer: '../app/bower_components/jwplayer-mirror/jwplayer.flash.swf',
         file: "../images/FunnyCatJumpFail.mp4",
         image: "../images/FunnyCatJumpFail.mp4",
+        width: 480,
+        height: 270,
+    });
+
+    // Controler la vidéo avec la barre d'espace
+    $(document).keydown(function(evt) {
+	    if (evt.keyCode == 32 && playerInstance.getState() == "PLAYING") {
+			playerInstance.pause();
+	    }else if(evt.keyCode == 32 && playerInstance.getState() == "PAUSED" || playerInstance.getState() == "IDLE"){
+	    	playerInstance.play();
+	    }
+  	});
+
+    // Callbacks de la vidéo
+    playerInstance.onReady(function(){
+
+    	console.log('ready');
+
+    	var video = document.querySelector('video');
+    	var canvas = document.querySelector('canvas');
+    	var context = canvas.getContext('2d');
+    	var w = 480;
+    	var h = 270;
+    	var ratio;
+
+    	// calcul du ratio
+		ratio = w / h;
+		// Taille canvas en respectant le ratio
+		w = w - 300;
+		h = parseInt(w / ratio, 10);
+		canvas.width = w;
+		canvas.height = h;
+
+    	playerInstance.onPlay(function(){
+    		console.log('playing');
+    		console.log('position : '+playerInstance.getPosition());
+    		console.log('Durée : '+playerInstance.getDuration());
+    	});
+
+    	playerInstance.onPause(function(){
+    		console.log('paused');
+    		console.log('position : '+playerInstance.getPosition());
+    	});
+
+    	function snapShot(){
+
+	    	context.fillRect(0,0,w,h);
+	    	context.drawImage(video, 0, 0, w, h);
+
+    	}
+
+    	$( ".capture" ).click(function() {
+			snapShot();
+		});
+
     });
 
 	// Récupération d'une vidéo youtube
@@ -21,6 +77,6 @@ $( document ).ready(function() {
 		        image: videoImage,
 		    });
 		}
-	});	
+	});
 
 });
